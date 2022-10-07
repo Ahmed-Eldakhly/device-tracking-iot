e5f8e7fa1f744a021cfd0b880a42680d4eb41a61
Ahmed-Eldakhly <ahmedeldakhlly94@gmail.com>
2022-10-08 01:56:58 +0200

    create the project

---
+++ src/main/java/com/vodafone/vodafonetask/VodafoneTaskApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.vodafonetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneTaskApplication.class, args);
+	}
+
+}
